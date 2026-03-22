38d49e66e4cc4673d18c16491a8d8bc2b25baa11
BuildTools <unconfigured@null.spigotmc.org>
2026-03-22 23:30:16 +0100

    Állapotgép akcióban

---
+++ skeleton/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Üdvözöllek a szkeletonban!\n eddig még semmi se készült el, de ez a felirat azt jelenti hogy működik a java:3");
+        System.out.println("O  O");
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(" ||");
+        }
+        System.out.println(" ()");
+        System.out.println("  ░");
+        System.out.println("   ░");
+        System.out.println("    ░");
+    }
+}
